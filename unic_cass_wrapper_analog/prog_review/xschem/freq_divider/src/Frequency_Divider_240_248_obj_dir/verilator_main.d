4f851de7db7c763986d7468c6120b6bf98f5a2c4
Avishka Herath <achamuditha@gmail.com>
2026-04-08 17:38:05 +0530

    Updating repo folder structure

--- unic_cass_wrapper_analog/prog_review/xschem/freq_divider/src/Frequency_Divider_240_248_obj_dir/verilator_main.d
+++
@@ -1,4 +0,0 @@
-verilator_main.o: /usr/share/ngspice/scripts/src/verilator_main.cpp \
- /usr/share/ngspice/scripts/src/ngspice/cmtypes.h \
- /usr/share/ngspice/scripts/src/ngspice/miftypes.h \
- /usr/share/ngspice/scripts/src/ngspice/cosim.h
